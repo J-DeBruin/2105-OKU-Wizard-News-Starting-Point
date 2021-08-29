b9ef0137f41a6194facb04de0e388fc05e81c2b8
Justin <83990960+J-DeBruin@users.noreply.github.com>
2021-08-29 10:43:36 -0500

    Wizard News Connection

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+
+// app.get("/", (req, res) => {
+//   const posts = postBank.list();
+
+app.get("/posts/:id", (req, res) => {
+  const id= req.params.id;
+  const post = postBank.find(id);
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`
+
+    res.send(html);
+});
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
